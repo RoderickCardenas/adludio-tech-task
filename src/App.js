e7af638bd249c55d16819509657c62d85a4999f1
Roderick Cardenas <rod@rodericks-mbp.home>
2019-11-29 17:00:31 +0000

    created loginDiv

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import ImageDiv from './components/imageDiv'
+import Login from './components/Login'
 
 const App = () => {
   return (
     <section className='container'>
       <ImageDiv />
+      <Login />
     </section>
   )
 }
---
+++ src/components/Login.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='loginDiv'>
+      <label>Full Name</label>
+      <input type='text' />
+      <label>E-mail</label>
+      <input type='text' />
+      <label>Password</label>
+      <input type='password' />
+    </div>
+  )
+}
+
+export default Login
