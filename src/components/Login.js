0cd74129f8a290cb5141c44f047319c08ca800b9
Roderick Cardenas <rod@rodericks-mbp.home>
2019-11-29 18:25:47 +0000

    completed left hand side of div except font style

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,6 +4,11 @@ import { GoogleLogin } from 'react-google-login'
 const Login = () => {
   return (
     <div className='loginDiv'>
+      <img
+        className='adludio-logo'
+        src='https://bannersforapp.s3.eu-west-2.amazonaws.com/adludio-assets/asset3.png'
+        alt=''
+      />
       <h3>Register</h3>
       <label className='login-label'>Full Name</label>
       <input className='login-input' type='text' />
--- src/components/imageDiv.js
+++ src/components/imageDiv.js
@@ -3,15 +3,17 @@ import React from 'react'
 const ImageDiv = () => {
   return (
     <div className='image-div'>
-      <h2 className='imageDiv-text'>REDIFINING MOBILE ADVERTISING</h2>
-      <h4 className='imageDiv-text'>Please use this form to register.</h4>
-      <h4 className='imageDiv-text'>
-        If you are a member, please{' '}
-        <a className='imageDiv-text' href='url'>
-          login
-        </a>
-        .
-      </h4>
+      <div className='imageDiv-text-container'>
+        <h2 className='imageDiv-text'>REDIFINING MOBILE ADVERTISING</h2>
+        <h4 className='imageDiv-text'>Please use this form to register.</h4>
+        <h4 className='imageDiv-text'>
+          If you are a member, please{' '}
+          <a className='imageDiv-text' href='url'>
+            login
+          </a>
+          .
+        </h4>
+      </div>
     </div>
   )
 }
