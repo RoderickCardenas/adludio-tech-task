a177fbb5b37367a7218242279c8ba67b41c13f9a
Roderick Cardenas <rod@rodericks-mbp.lan>
2019-12-01 10:13:23 +0000

    complete ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from 'react'
 import './App.css'
-// import ImageDiv from './components/imageDiv'
+import TextDiv from './components/TextDiv'
 import Login from './components/Login'
 
 const App = () => {
   return (
     <section className='container'>
-      <div className='text'>
-        <div className='inner-text'>
-          <h1 className='imageDiv-text'>REDIFINING MOBILE ADVERTISING</h1>
-          <div className='imageDiv-p-text'>
-            <p>Please use this form to register.</p>
-            <p>
-              If you are a member, please <a href='url'>login</a>.
-            </p>
-          </div>
-        </div>
-      </div>
-      {/* <ImageDiv /> */}
+      <TextDiv />
       <Login />
     </section>
   )
